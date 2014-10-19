b0f5329ecb38c2ed3bebdc44b4a081c562e95e0e
Martin J <martinnjorgensen@gmail.com>
2014-10-19 11:05:09 +0200

    Lesson 03 completed.
    Camera now follows player without to much fuzzing about.

---
+++ Roll_A_Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+    private Vector3 _offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _offset = transform.position;
+	}
+	
+	
+	void LateUpdate ()
+	{
+	    transform.position = Player.transform.position + _offset;
+	}
+}
