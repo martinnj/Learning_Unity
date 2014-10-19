bf5c86dba57a4be2e0cd2f1447561fa0d6d50b12
Martin J <martinnjorgensen@gmail.com>
2014-10-19 10:53:35 +0200

    Lesson 02 completed
    Player now moves.

---
+++ Roll_A_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Speed;
+    
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var movevertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, movevertical);
+
+	    rigidbody.AddForce(movement * Speed * Time.deltaTime);
+    }
+}
