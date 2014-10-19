3ca39a4f19d799e3603c144b89a8a04a6e570a70
Martin J <martinnjorgensen@gmail.com>
2014-10-19 14:06:25 +0200

    Phase 03 compelted.
    Camera tracks player.

---
+++ Nightmares/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Target;
+    public float Smoothing = 5f;
+
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - Target.position;
+    }
+
+    // Physics steps.
+    void FixedUpdate()
+    {
+        var targetCamPos = Target.position + _offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, Smoothing * Time.deltaTime);
+    }
+}
