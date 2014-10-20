027c3c5c926780ccfb526898e6546dd74e4be88b
Martin J <martinnjorgensen@gmail.com>
2014-10-20 22:26:49 +0200

    Basic Powerup system implemented.
    Still no drop system though :)

---
+++ Nightmares/Assets/Scripts/PickUps/PowerUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum PType
+    {
+        AttackSpeed,
+        MovementSpeed,
+        Health
+    };
+
+    public PType PowerAffects;  // What sort of powerup is this?
+    public Color PColor;        // Base color of the powerup material.
+    public float ModifierValue; // Value to modify with.
+    public float LifeTime;      // How long the powerup stays alive if not picked up.
+    public float LastsFor;      // How long the powerup will affect the player.
+
+    private float _alphaStep;
+
+    // Use this for initialization
+    void Start ()
+    {
+        renderer.material.color = PColor;
+        _alphaStep = PColor.a / LifeTime;
+        Debug.Log("Alpha Falloff rate:" + _alphaStep);
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    // Called once every physics tick.
+    void FixedUpdate()
+    {
+        PColor.a -= _alphaStep;
+        renderer.material.color = PColor;
+
+        LifeTime -= Time.deltaTime;
+
+        // Dispose of the PU if it's not picked up.
+        if (LifeTime != 0) return;
+        Destroy(transform.gameObject);
+    }
+
+    void PrintVars()
+    {
+        Debug.Log("ModifierValue: " + ModifierValue + "\n" + 
+                  "PColor: " + PColor + "\n" +
+                  "LifeTime: " + LifeTime + "\n" +
+                  "EffectTimer: " + LastsFor + "\n" +
+                  "_alphaStep: " + _alphaStep);
+    }
+}
--- Nightmares/Assets/Scripts/Player/PlayerMovement.cs
+++ Nightmares/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,12 +12,50 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _playerRigidbody;
     private int _floorMask;
     private const float CamRayLength = 100f;
+    private List<Tuple<PowerUp.PType, float, float>> _activeEffects;
 
     void Awake()
     {
         _floorMask = LayerMask.GetMask("Floor");
         _anim = GetComponent<Animator>();
         _playerRigidbody = GetComponent<Rigidbody>();
+        _activeEffects = new List<Tuple<PowerUp.PType, float, float>>();
+    }
+
+    // Called when we "Enter a trigger area".
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "PowerUp") return;
+
+        // Get variables from the powerup.
+        var powerup = other.GetComponent<PowerUp>();
+        var type = powerup.PowerAffects;
+        var time = powerup.LastsFor;
+        var modval = powerup.ModifierValue;
+
+        // Apply them, and save if applicable.
+        switch (type)
+        {
+            case PowerUp.PType.AttackSpeed:
+                var player = GameObject.FindGameObjectWithTag("Player");
+                var gscript = player.transform.Find("GunBarrelEnd").GetComponent<PlayerShooting>();
+                gscript.TimeBetweenBullets -= modval;
+                _activeEffects.Add(new Tuple<PowerUp.PType, float, float>(type, time, modval));
+                break;
+
+            case PowerUp.PType.Health:
+                var hscript = transform.GetComponent<PlayerHealth>();
+                hscript.CurrentHealth += (int) modval;
+                break;
+
+            case PowerUp.PType.MovementSpeed:
+                Speed += modval;
+                _activeEffects.Add(new Tuple<PowerUp.PType, float, float>(type, time, modval));
+                break;
+        }
+
+        // Remove powerop from game
+        other.gameObject.SetActive(false);
     }
 
     // Physics based updates.
@@ -25,8 +66,10 @@ public class PlayerMovement : MonoBehaviour
         Move(h, v);
         Turning();
         Animating(h, v);
+        ProcessPowerUps();
     }
 
+    // Move Player around the world.
     void Move(float h, float v)
     {
         _movement.Set(h, 0f, v);
@@ -35,6 +78,7 @@ public class PlayerMovement : MonoBehaviour
         _playerRigidbody.MovePosition(transform.position + _movement);
     }
 
+    // Rotate the Player to aim at the mouse.
     void Turning()
     {
         var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -48,10 +92,69 @@ public class PlayerMovement : MonoBehaviour
         _playerRigidbody.MoveRotation(newRotation);
     }
 
+    // Control the walking animation.
     void Animating(float h, float v)
     {
         var walking = h != 0f || v != 0f; //TODO: Floating point comparisons are bad :(
         _anim.SetBool("IsWalking", walking);
     }
 
+    void ProcessPowerUps()
+    {
+        var expiredEffects = new List<Tuple<PowerUp.PType, float, float>>();
+        foreach (var powerup in _activeEffects)
+        {
+            // Process effect
+            var type = powerup.First;
+            var life = powerup.Second;
+            var modval = powerup.Third;
+
+            powerup.Second = life - Time.deltaTime;
+
+            if (life > 0) continue;
+            // Remove the effects.
+            switch (type)
+            {
+                // Increase cooldown of shooting.
+                case PowerUp.PType.AttackSpeed:
+                    var player = GameObject.FindGameObjectWithTag("Player");
+                    var gscript = player.transform.Find("GunBarrelEnd").GetComponent<PlayerShooting>();
+                    gscript.TimeBetweenBullets += powerup.Third;
+                    break;
+
+                case PowerUp.PType.MovementSpeed:
+                    Speed -= modval;
+                    break;
+            }
+            // Remove powerup from active effects list.
+            expiredEffects.Add(powerup);
+        }
+        foreach (var powerup in expiredEffects)
+        {
+            _activeEffects.Remove(powerup);
+        }
+    }
+
+}
+
+public class Tuple<T1, T2, T3>
+{
+    public T1 First { get; set; }
+    public T2 Second { get; set; }
+    public T3 Third { get; set; }
+    internal Tuple(T1 first, T2 second, T3 third)
+    {
+        First = first;
+        Second = second;
+        Third = third;
+    }
+}
+
+public static class Tuple
+{
+    public static Tuple<T1, T2, T3> New<T1, T2, T3>(T1 first, T2 second, T3 third)
+    {
+        var tuple = new Tuple<T1, T2, T3>(first, second, third);
+        return tuple;
+    }
 }
